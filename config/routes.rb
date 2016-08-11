960f8a6d78426f469d66f05b9f4d58791a090af9
romainsimoes <romain.simoes.pro@gmail.com>
2016-08-11 11:01:50 +0200

    Root to modif

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :cocktails
 
-  root, to: 'cocktails#index'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
